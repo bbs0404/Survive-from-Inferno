2b69b04c2acc833c15d8140f5e9538d0d7b26fa4
peridote <3rlatpghks3@naver.com>
2017-01-10 17:45:50 +0900

    icecream?

--- Inferno/Assets/Scripts/Item/Icecream.cs
+++ Inferno/Assets/Scripts/Item/Icecream.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Icecream : Item {
-
+    public int water;
+    public int health;
 	public void use()
     {
-
+        InGameSystemManager.Inst().water = Mathf.Max(InGameSystemManger.Inst().water + water, 100f);
+        InGameSystemManager.Inst().health = Mathf.Max(InGameSystemManager.Inst().health + health, InGameSystemManager.Inst().maxHealth);
     }
 }
